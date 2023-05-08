a344274bf6f8a36e056a20cf836b0dc217222942
Akash NP <akashnp1998@gmail.com>
2023-05-08 22:02:57 +0530

    feat: integrate fastapi, add core endpoints (create embeddings & ask question)

--- tbhp_riding_gear_assitant/main.py
+++ tbhp_riding_gear_assitant/main.py
@@ -1,6 +1,37 @@
-from core.comments_loader import TBHPCommentsLoader
-from utils.functions import create_embeddings
+from fastapi import FastAPI
+from langchain.llms import OpenAI
 
-loader = TBHPCommentsLoader().get_instance()
-docs = loader.get_comment_documents(n=10)
-create_embeddings(docs=docs)
+from api.models import Query
+from core.comments_loader import TBHPCommentsLoader
+from core.prompts import prompt_template
+from utils.functions import create_embeddings, query_db
+
+app = FastAPI()
+llm = OpenAI()
+
+@app.get('/api/create-embeddings/')
+def generate_embeddings(n_page: int):
+    """
+    create embeddings of the comments from page 1
+    to n_page of the "TBHP Riding Gear" Thread
+    """
+    loader = TBHPCommentsLoader().get_instance()
+    docs = loader.get_comment_documents(n=n_page)
+    create_embeddings(docs=docs)
+    return {"message": "embeddings created successfuly"}
+
+@app.post('/api/query/')
+def ask_query(payload: Query):
+    """
+    core endpoint that takes query from user, queries
+    context via similarity search and passes the result(s)
+    along with the question to OpenAI.
+
+    Args:
+        payload (Query): a JSON object with "query" key
+        containing the actual user query
+    """
+    query = payload.query
+    context = query_db(query=query)
+    prompt = prompt_template.format(context=" ".join(context), question=query)
+    return {"response": llm(prompt)}
